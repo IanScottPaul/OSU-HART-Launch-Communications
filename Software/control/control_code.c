9be15766fdf8bf9e57f280c4b3375755d7f2c41a
IanScottPaul <paulia@oregonstate.edu>
2021-02-12 18:13:17 -0800

    starting software

---
+++ Software/control/control_code.c
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////
+// FILE:	control_code.c 			//
+// DATE:	Winter 2021			//
+// DEVELOPER:	Ian Scott Paul			//
+// TEAM:	High Altitude Rocket Team	//
+// PROGRAM:	Launch Communications Software	//
+// VER.:	CONTROL-0.1			//
+// PROJECT:	HART GROUND SYSTEMS		//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DISCLAIMER: 	The Launch Communications Software is deployed on the 2021 Launch Communications PCBs. 	This software is written to specifically target these boards, but should function for other similar AVR systems with little configuration. See schematic in repository. 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// USE THE INCLUDED MAKEFILE AND AVR 6 PIN ICSP TO PROGRAM LAUNCH COMMUNICATIONS PCBS //
+////////////////////////////////////////////////////////////////////////////////////////
+////INCLUDED LIBRARIES////
+#include <avr/io.h>
+
+////MACROS////
+
+
+////ALT. DATA TYPES////
+
+////FUCNTION DECLARATIONS////
+void CONTROL_GPIO_INIT();
+
+////INTERRUPT VECTORS////
+
+
+
+
+uint8_t main(){	
+	////Program Initialization////
+		//initialize GPIO 
+		//INIT SPI
+		//INIT TIMERS
+	while(1){			//Endless while loop
+		//STATE MACHINE
+		//RF WATCHDOG
+		//
+		//
+		//
+		
+		
+		
+	}//while_1
+}//main
